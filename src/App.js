34fe2fdac3a0ff44cf9f148adc1225c3676e2edb
jorge0730 <jorgepardo0730@hotmail.com>
2024-04-29 14:40:30 -0500

    cards, uso de hooks, crear componentes en react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Menu from './componentes/menu';
+import Footer from './componentes/footer';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Menu/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
